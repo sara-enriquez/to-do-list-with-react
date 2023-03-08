3fba63831a9c0c70d0d42faf3033154ffdc3e636
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-03-08 18:29:45 +0000

    create handleDone function

--- src/js/component/Formulario.jsx
+++ src/js/component/Formulario.jsx
@@ -1,26 +1,34 @@
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 
 const Formulario = (props) => {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState();
+  const [counter, setCounter] = useState(1);
 
-  const handleDown = (e) => {
-    if (e.keyCode == 13) {
-      props.addNewTask(task);
-      console.log(task);
+  useEffect(() => {
+    if (task !== undefined) {
+      setCounter((counter) => counter + 1);
+      let object = {
+        label: props.taskDone,
+        id: counter,
+        body: task,
+      };
+      props.setTaskList(props.taskList.concat(object));
     }
-  };
-  const handleChange = (e) => {
-    setTask(e.target.value);
-  };
+  }, [task]);
 
   return (
     <input
-      onChange={handleChange}
       className="input-task"
       type="text"
+      id="tarea"
       placeholder="Escribir nueva tarea"
-      onKeyDown={handleDown}
-      value={task}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" && document.getElementById("tarea").value !== "") {
+          setTask(e.target.value);
+          tarea.value = "";
+        }
+      }}
     />
   );
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Formulario from "./Formulario.jsx";
 
-const Home = () => {
-  const [taskList, setTaskList] = useState(["sacar la basura"]);
+const Home = (props) => {
+  const [taskList, setTaskList] = useState([]);
+  const [deleted, setDeleted] = useState();
+  const [taskDone, setTaskDone] = useState(false);
+  
+  let doneClass = "fa-solid fa-circle-check";
+  let undoneClass = "fa-regular fa-circle-check";
 
-  const addNewTask = (task) => {
-    setTaskList([...taskList, task]);
-	
+  const handleDone = (i) => {
+    let copyArray = [...taskList];
+    copyArray[i].label = !copyArray[i].label;
+    setTaskList(copyArray);
   };
+
+  useEffect(() => {
+    if (deleted) {
+      let newArray = taskList.filter((tarea) => tarea.id != deleted);
+      setTaskList(newArray);
+    }
+  }, [deleted]);
 
   return (
     <div className="content-home">
       <h1>Tareas Pendientes</h1>
       <div className="content-form">
-        <Formulario addNewTask={addNewTask} />
+        <Formulario
+          taskList={taskList}
+          setTaskList={setTaskList}
+          taskDone={taskDone}
+          setTaskDone={setTaskDone}
+        />
       </div>
       <div className="content-tasks">
         <ul className="list">
-          <li>{taskList}</li>
+          {taskList.map((task, i) => {
+            return (
+              <li className={task.label ? "liDone" : "liUndone"}>
+                {task.body}
+                <i
+                  className="fa-regular fa-circle-xmark"
+                  onClick={() => setDeleted(task.id)}
+                ></i>
+                <i
+                  className={task.label ? doneClass : undoneClass}
+                  onClick={() => {
+                    handleDone(i);
+                  }}
+                ></i>
+              </li>
+            );
+          })}
+          <li className={taskList.length ? "liUndone" : "liDone"}>{`${taskList.length} tareas pendientes`}</li>
         </ul>
       </div>
     </div>
